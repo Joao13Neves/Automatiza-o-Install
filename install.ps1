55f1b88105f560205fc2aad89202b97a82daeac1
Joao13Neves <89791399+Joao13Neves@users.noreply.github.com>
2021-10-11 17:20:48 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -17,8 +17,8 @@ choco upgrade chocolatey
 Write-Host "Installing Google Chrome"
 cinst googlechrome --version=94.0.4606.81 -Y
 
-Write-Host "Installing Java 8 (JDK)"
-cinst jdk8 --version=8.0.211 -Y
+Write-Host "Installing Java (JDK)"
+cinst openjdk --version=11.0.2.01 -Y
 
 Write-Host "Installing Winrar"
 cinst winrar -Y
